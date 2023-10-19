d44fb9b3698897d0ae50adc9e69c172c4b6ec08e
unknown <3054117680@qq.com>
2023-10-19 20:30:17 +0800

    Lab1:update

--- Spinning-Pinwheel/src/application.cpp
+++ Spinning-Pinwheel/src/application.cpp
@@ -30,6 +30,8 @@ double Acceleration = 0.3;
 
 
 
+
+
 void reverse()
 {
     direction = -direction;
@@ -61,16 +63,18 @@ void rotate()
 
 //button part
 
-void printCharater(const char* str)
+void printCharater(const char* str,float labelX,float labelY)
 {
-    glRasterPos2f(-0.06, 0.74);
+    glRasterPos2f(labelX, labelY);
     for (int i = 0; i < strlen(str); i++) glutBitmapCharacter(GLUT_BITMAP_8_BY_13, *(str + i));
 }
+
 struct button
 {
     char* str = new char[100];
     bool cleck;
-    void rev() {
+    void showButton(int r, int g, int b, const char* b_label,float labelX,float labelY,
+        float top, float bottom, float left, float right) {
 
         glPushMatrix();
 
@@ -78,15 +82,15 @@ struct button
         {
             glScalef(0.98, 0.98, 1.0);
         }
-        //button
-        glColor3f(0, 0, 0);
-        printCharater("Reverse");
+        glColor3f(r, g, b);
+        printCharater(b_label,labelX,labelY);
         glColor3f(1, 1, 1);
         glBegin(GL_QUADS);
-        glVertex3f(-0.1, 0.8, 0);
-        glVertex3f(0.1, 0.8, 0);
-        glVertex3f(0.1, 0.7, 0);
-        glVertex3f(-0.1, 0.7, 0);
+        glVertex3f(left, top, 0);
+        glVertex3f(right, top, 0);
+        glVertex3f(right, bottom, 0);
+        glVertex3f(left, bottom, 0);
+
         glEnd();
 
 
@@ -95,9 +99,15 @@ struct button
 
 
 };
-button* pBtn;
+button *reverseBtn,*speedUpBtn,*speedDownBtn;
+
+
 void mouseFunc(GLint btn, GLint sta, int x, int y)
 {
+    int rxl, rxr, ryb, ryt,
+        suxl,suxr,suyb,suyt,
+        sdxl,sdxr,sdyb,sdyt;
+
 
     if (btn == GLUT_LEFT_BUTTON)
         switch (sta)
@@ -106,24 +116,21 @@ void mouseFunc(GLint btn, GLint sta, int x, int y)
         {
             if (x >= 360 && x <= 440 && y >= 40 && y <= 80) {
                 reverse();
-                pBtn->cleck = true;
+                reverseBtn->cleck = true;
             }
             break;
         }
         case GLUT_UP:
         {
-            pBtn->cleck = false;
+            reverseBtn->cleck = false;
         }
         }
-    if (x >= 360 && x <= 440 && y >= 40 && y <= 80) {
-        reverse();
-    }
     glutPostRedisplay();
 }
 
 
 //menu part
-void subFunc(GLint data)
+void EntryAction(GLint data)
 {
     switch (data)
     {
@@ -134,10 +141,10 @@ void subFunc(GLint data)
 }
 
 
-void creatMenu()
+void createMenu()
 {
     //创建菜单
-    GLint sub = glutCreateMenu(subFunc);
+    GLint sub = glutCreateMenu(EntryAction);
     glutAddMenuEntry("Speed Up", 1);
     glutAddMenuEntry("Speed Down", 2);
     glutAddMenuEntry("Reverse", 3);
@@ -151,15 +158,14 @@ void keyBoard(unsigned char key, int x, int y)
 {
     switch (key)
     {
-    case 'q':reverse(); break;//key "q" for reverse
-    case 'w':speedUp(); break;//key "w" for speed up
-    case 's':speedDown(); break;//key "s" for speed down
+    case 'q':reverse(); break;
+    case 'w':speedUp(); break;
+    case 's':speedDown(); break;
     case 27:exit(0);
     }
 }
 
 
-//drawing part
 void drawPinwheel()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -167,7 +173,7 @@ void drawPinwheel()
     glPushMatrix();
     glRotatef(angle, 0, 0, 1);
     glScalef(1, 1, 1);
-    //中心黄色部分
+    //中轴
     glColor3f(1, 1, 0);
     glBegin(GL_TRIANGLES);
     glVertex3f(-0.1f, -0.2 * sqrt(3) / 6, 0);
@@ -175,7 +181,7 @@ void drawPinwheel()
     glVertex3d(0.0f, 0.1 * sqrt(3) - 0.2 * sqrt(3) / 6, 0);
     glEnd();
 
-    //red wind
+    //红扇叶
     glColor3f(1, 0, 0);
     glBegin(GL_TRIANGLES);
     glVertex3f(-0.1f, -0.2 * sqrt(3) / 6, 0);
@@ -191,7 +197,7 @@ void drawPinwheel()
     glVertex3d(0.1 * 27 / 52, -0.2 * 147 / 113 / 146 * 230 - 0.2 * sqrt(3) / 6, 0);
     glEnd();
 
-    //blue wind
+    //蓝扇叶
     glRotatef(120, 0, 0, 1);
     glColor3f(0, 0, 1);
     glBegin(GL_TRIANGLES);
@@ -208,7 +214,7 @@ void drawPinwheel()
     glVertex3d(0.1 * 27 / 52, -0.2 * 147 / 113 / 146 * 230 - 0.2 * sqrt(3) / 6, 0);
     glEnd();
 
-    //green wind
+    //绿扇叶
     glRotatef(120, 0, 0, 1);
     glColor3f(0, 1, 0);
     glBegin(GL_TRIANGLES);
@@ -227,7 +233,7 @@ void drawPinwheel()
 
     glPopMatrix();
 
-    //bar
+    //风车把
     glScalef(1, 1, 1);
     glColor3f(1, 0, 1);
     glBegin(GL_POLYGON);
@@ -240,7 +246,9 @@ void drawPinwheel()
     glPopMatrix();
 
     //button 
-    pBtn->rev();
+    speedUpBtn->showButton(0,0,0,"Speed Up", -0.52,0.74,0.8, 0.7, -0.6, -0.3);
+    reverseBtn->showButton(0, 0, 0, "Reverse",-0.06,0.74, 0.8, 0.7, -0.1, 0.1);
+    speedDownBtn->showButton(0, 0, 0, "Speed Down", 0.36,0.74,0.8, 0.7, 0.3, 0.6);
 
     glPopMatrix();
 
@@ -264,21 +272,25 @@ void Init()
     glLoadIdentity();
     gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
     glShadeModel(GL_SMOOTH);
-    pBtn = new button;
-    pBtn->cleck = false;
+    reverseBtn = new button;
+    reverseBtn->cleck = false;
+    speedUpBtn = new button;
+    speedUpBtn->cleck = false;
+    speedDownBtn = new button;
+    speedDownBtn->cleck = false;
 }
 
-int main(int argc, char* argv[]) //
+int main(int argc, char* argv[]) 
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(700, 700);
-    glutInitWindowPosition(300, 100);
+    glutInitWindowPosition(100, 100);
     glutCreateWindow("Spinning Pinwheel");
     glEnable(GL_DEPTH_TEST | GL_LINE_SMOOTH | GL_POLYGON_SMOOTH | GL_POLYGON_STIPPLE);
 
     Init();
-    creatMenu();
+    createMenu();
     glutDisplayFunc(&drawPinwheel);
     glutIdleFunc(&rotate);
     glutKeyboardUpFunc(&keyBoard);
